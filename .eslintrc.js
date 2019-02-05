9b5d54490bab082c277a3c323036c12d7a2e4446
eunjin kim <uwangg@gmail.com>
2019-02-05 18:09:07 +0900

    eslint rules 추가

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,13 @@
 module.exports = {
-    "extends": "standard"
+    "extends": "standard",
+    "rules": {
+        "indent": [
+            "error",
+            4
+        ],
+        "semi": [
+            "error",
+            "always"
+        ],
+    }
 };
--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 const Koa = require('koa');
-const app = new Koa;
+const app = new Koa();
 
 app.use(ctx => {
     ctx.body = 'hello world';
-})
+});
 
 app.listen(3003);
